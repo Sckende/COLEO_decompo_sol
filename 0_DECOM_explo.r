a1dc04aeb37de8910c253ac9921d7dc6c156bb29
Sckende <clrccljuhasz@gmail.com>
2025-01-20 16:11:05 -0500

    explo

--- 0_DECOM_explo.r
+++ 0_DECOM_explo.r
@@ -1,7 +1,14 @@
-#### Decomposition sol - Complete data gabarit ####
-decom <- read.csv("/home/local/USHERBROOKE/juhc3201/BdQc/COLEO/Data/2025-01-16_decomposition_sol-complete_data.csv",
-    h = TRUE
+# ---------------------------- #
+#### Decomposition sol - Complete data gabarit from Coleo ####
+# ---------------------------- #
+
+decom <- read.csv("/home/local/USHERBROOKE/juhc3201/BdQc/COLEO/Data/COLEO_decompo_sol/2025-01_decomposition_sol-donnees_completes.csv",
+    header = TRUE,
+    sep = ",",
+    dec = "."
 )
+library(readxl)
+decom2 <- read_excel("/home/local/USHERBROOKE/juhc3201/BdQc/COLEO/Data/COLEO_decompo_sol/2025-01_decomposition_sol-donnees_completes.xlsx")
 head(decom)
 summary(decom)
 sapply(decom, class)
@@ -10,17 +17,19 @@ names(decom)
 col <- c("obs_soil_decomposition_start_weight", "obs_soil_decomposition_end_weight_with_bag", "obs_soil_decomposition_end_weight_tea", "obs_soil_decomposition_shading")
 decom[col] <- sapply(decom[col], as.numeric)
 
-#### Decomposition sol - simplified data & calculated indices ####
-s_dec <- read.csv("/home/local/USHERBROOKE/juhc3201/BdQc/COLEO/Data/2025-01-16_decomposition_sol-simple_data.csv",
-    h = TRUE, row.names = NULL
-)
-head(s_dec)
-names(s_dec) <- c("region", "site_name", "site_type", "site_code", "lat_centro", "lon_centro", "date_pose", "date_recup", "jour_sol", "v_decomp", "fact_stab", "pds_ini_tv", "pds_fin_sac_tv", "pds_fin_tv", "pds_ini_rooi", "pds_fin_sac_rooi", "pds_fin_rooi")
-summary(s_dec)
+# ---------------------------- #
+#### Decomposition sol - simplified data & calculated indices from Coleo ####
+# ---------------------------- #
 
-table(s_dec$site_type)
-summary(s_dec$jour_sol)
-hist(s_dec$jour_sol[s_dec$site_type == "toundrique"])
+s_decom <- read.csv("/home/local/USHERBROOKE/juhc3201/BdQc/COLEO/Data/COLEO_decompo_sol/2025-01_decomposition_sol-simple_data.csv",
+    h = TRUE, dec = "."
+)
+head(s_decom)
+summary(s_decom)
+
+table(s_decom$site_type)
+summary(s_decom$jour_sol)
+hist(s_decom$jour_sol[s_decom$site_type == "toundrique"])
 
 #### Interactive ggplot - lat vs v_decomp ####
 library(ggplot2)
@@ -30,6 +39,8 @@ library(plotly)
 library(viridis)
 library(hrbrthemes)
 library(sf)
+library(lubridate)
+
 
 # Text preparation
 p <- s_dec %>%
@@ -124,3 +135,49 @@ summary(lm(p$fact_stab ~ p$lat_centro))
 summary(lm(p$v_decomp ~ p$lat_centro))
 summary(lm(p$v_decomp ~ p$site_type))
 summary(lm(p$fact_stab ~ p$site_type))
+
+# ------------------------------------------------ #
+#### MELCCFP data & comparison with coleo data ####
+# ---------------------------------------------- #
+
+melccfp <- read.csv("/home/local/USHERBROOKE/juhc3201/BdQc/COLEO/Data/COLEO_decompo_sol/MELCCFP_raw_data.csv", header = TRUE, dec = ".")
+
+# Class conversion
+col <- c("stabilisation", "decomposition")
+melccfp[col] <- sapply(melccfp[col], as.numeric)
+
+summary(melccfp)
+# retrieve basal information
+infos <- decom2[, c("sites_site_code", "landmarks_lat", "landmarks_lon", "campaigns_opened_at", "campaigns_closed_at", "obs_soil_decomposition_date_end", "obs_soil_decomposition_bag_no")]
+
+melccfp2 <- left_join(melccfp, infos, by = join_by(vert == obs_soil_decomposition_bag_no))
+
+melccfp3 <- melccfp2[!is.na(melccfp2$sites_site_code), ]
+dim(melccfp2)
+sites_melccfp <- unique(melccfp2$sites_site_code)
+sites_coleo <- unique(s_decom$site_code)
+
+length(sites_melccfp)
+length(sites_coleo)
+extra_sites <- sites_coleo[!(sites_coleo %in% sites_melccfp)]
+
+# retrait des sites non présents dans les donnees du melccfp
+s_decom2 <- s_decom[!(s_decom$site_code %in% extra_sites), ]
+names(s_decom2)
+names(melccfp2)
+
+melccfp3$campaigns_opened_at <- ymd(melccfp3$campaigns_opened_at)
+melccfp3$campaigns_closed_at <- ymd(melccfp3$campaigns_closed_at)
+melccfp3$obs_soil_decomposition_date_end <- ymd(melccfp3$obs_soil_decomposition_date_end)
+melccfp3$site_campaigns_year <- paste0(melccfp3$sites_site_code, "-", year(melccfp3$campaigns_opened_at))
+
+lis <- split(melccfp3, melccfp3$site_campaigns_year)
+length(lis)
+#### *** restart here - mean of v and s per site per year *** ####
+# ---------------------------- #
+#### Environmental data ####
+# ---------------------------- #
+
+# from CHELSA - bio1 (mean annual air temperature), bio5 (mean daily maximum air temperature of the warmest month), bio6 (mean daily minimum air temperature of the coldest month), bio12 (annual precipitation amount) & GHMTS (Global Human of Terrestrial Systems)
+env <- readRDS("/home/local/USHERBROOKE/juhc3201/BdQc/COLEO/Data/env_data/site_climate.RDS")
+head(env)
